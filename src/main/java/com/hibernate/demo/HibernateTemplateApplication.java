97ff16cb42450f914a3782e50f6a618b5ccbac03
sing-fung <1587898817@qq.com>
2023-01-22 17:05:10 -0500

    initialize project

---
+++ src/main/java/com/hibernate/demo/HibernateTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateTemplateApplication.class, args);
+    }
+
+}
